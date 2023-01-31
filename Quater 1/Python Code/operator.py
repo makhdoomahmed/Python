ba775549a1295d46ce2c8c5cd6dd649a8aba102f
Syed Makhdoom Ahmed Ashraf <63533420+makhdoomahmed@users.noreply.github.com>
2023-01-31 19:13:26 +0400

    Add files via upload

---
+++ Quater 1/Python Code/operator.py
@@ -0,0 +1,15 @@
+#print (10+90)
+#print (100-90)
+#print (6/2)
+#print (2*3)
+#print (13%2)
+# print (6//2)
+# print (2**3)
+
+#print (3**2/2*3/3+6-4) #PEMDAS
+#parantes exponces multiply division addtion subsctraction
+# left to right  sequence for M D & A S
+
+name = input("What is your name  ?")
+age = input ("""How old are you ? {name}""")
+print ("You are still young. Only ", age)
